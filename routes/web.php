d3ec491295a07cebd56747316a445aafafad175f
Zeyad Sharo <33178745+zeyadsharo@users.noreply.github.com>
2022-03-15 23:44:24 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin');
 });
+Route::get('/test', function () {
+    return   Artisan::call('storage:link');
+});
